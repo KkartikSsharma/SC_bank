e19a44c7792ec5f3f19dedc6af4d2898cd7177f9
kartik <Kartik.Sharma@sc.com>
2019-12-14 16:08:34 +0530

    Changes from office one

--- src/test/resume.js
+++ src/test/resume.js
@@ -22,7 +22,7 @@ export default class Resume extends Component {
             showCertificates: false,
             showExtraCurriculum: false
         }
-
+        // window.print();
     }
     fetchDataHandler() {
         Axios.get("http://engineershub.org.in/engineers_hub_2/resume_download.php?operation=0&uid=23cfb2d1-4544-4e3c-add4-37e60551")
@@ -56,18 +56,16 @@ export default class Resume extends Component {
                         this.setState({ achieve: achieve })
                     }
                 }
-                var temp=[]
-                for(i=0; i<this.state.education.length-1;i++){
-                    if(this.state.education[i].to_time>this.state.education[i+1])
-                        {
-                            temp=this.state.education[i]
-                        }
-                    else
-                    {
-                        temp=this.state.education[i+1]
+                var temp = []
+                for (i = 0; i < this.state.education.length - 1; i++) {
+                    if (this.state.education[i].to_time > this.state.education[i + 1]) {
+                        temp = this.state.education[i]
+                    }
+                    else {
+                        temp = this.state.education[i + 1]
                     }
                 }
-                this.setState({education:temp})
+                this.setState({ education: temp })
                 if (this.state.trainings != "") {
                     this.setState({ showCertificates: true })
                 }
@@ -91,7 +89,7 @@ export default class Resume extends Component {
     }
     render() {
         return (
-            <div style={{ fontFamily: "montserrat", padding: "100px" }}>
+            <div className="col-sm-12 col-xs-12" style={{ fontFamily: "montserrat", padding: "50px" }}>
                 <div className="col-sm-12 col-xs-12" style={{ padding: "0px" }}>
                     <div className="col-sm-5 col-xs-5" style={{ padding: "0px", margin: "0px", marginTop: "25px", fontWeight: "bold" }}>
                         <p style={{ fontSize: "50px", color: "#177acc", textAlign: "left", borderBottom: "3px solid black", width: "100%", marginRight: "0px" }}>{this.state.cv.name}</p>
@@ -128,7 +126,6 @@ export default class Resume extends Component {
                                     <div>
                                         <div style={{ padding: "5px", paddingLeft: "0px", fontWeight: "bolder", fontSize: "25px" }}>{this.state.education.title}</div>
                                         <div style={{ padding: "5px", paddingLeft: "0px", fontWeight: "bold", fontSize: "15px" }}>{this.state.education.institute}</div>
-
                                         <div style={{ padding: "5px", paddingLeft: "0px" }}>{this.state.education.to_time}</div>
                                     </div>}
                             </div></div> : null}
@@ -166,7 +163,7 @@ export default class Resume extends Component {
                             </div>)}
 
                     </div></div> : null}
-                <div className="col-sm-12 col-xs-12 break" style={{ padding: "0px", marginTop: "30px" }}>
+                {/* <div className="col-sm-12 col-xs-12 break" style={{ padding: "0px", marginTop: "30px" }}>
                     {this.state.showCertificates ?
                         <div className="col-sm-12 col-xs-12" style={{ padding: "0px" }}>
                             <div className="col-sm-12 col-xs-12" style={{ fontSize: "30px", padding: "0", color: "#177acc", textAlign: "left", borderBottom: "3px solid black" }}>
@@ -199,6 +196,49 @@ export default class Resume extends Component {
                                 </div>)}
                         </div>
                     </div> : null}
+                </div> */}
+                <div className="col-sm-12 col-xs-12" style={{padding:"0px",paddingTop:"30px"}}>
+                    <table>
+                        <colgroup>
+                            <col span="1" style={{width: "33.33%"}} />
+                            <col span="1" style={{width: "33.33%"}}  />
+                            <col span="1" style={{width: "33.33%"}}  />
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <td style={{ fontSize: "30px", padding: "0px", paddingTop: "30px", color: "#177acc", textAlign: "left", borderBottom: "3px solid black" }}>
+                                    Certificates
+                            </td>
+                                <td />
+                                <td style={{ fontSize: "30px", padding: "0px", paddingTop: "30px", color: "#177acc", textAlign: "left", borderBottom: "3px solid black" }}>
+                                    Extra-Curricular Activities
+                            </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style={{verticalAlign:"top"}}>
+                                {this.state.trainings.map(i =>
+                                    <table style={{ marginBottom: "10px",pageBreakInside:"avoid" }}>
+                                        <tr><td style={{ padding: "5px", paddingLeft: "0px", fontWeight: "bolder", fontSize: "25px" }}>{i.title}</td></tr>
+                                        <tr><td style={{ padding: "5px", paddingLeft: "0px", fontWeight: "bold", fontSize: "15px" }}>{i.institute}</td></tr>
+                                        <tr><td style={{ padding: "5px", paddingLeft: "0px" }}>{i.description}</td></tr>
+                                        <tr><td style={{ padding: "5px", paddingLeft: "0px" }}>{i.from_time}-{i.to_time}</td></tr>
+                                    </table>)}
+                                </td>
+                                <td />
+                                <td style={{verticalAlign:"top"}}>
+                                    {this.state.extra.map(i =>
+                                        <table className="col-sm-12 col-xs-12" style={{ padding: "0px",pageBreakInside:"avoid" }}>
+                                            <tr ><td style={{ padding: "5px", paddingLeft: "0px", fontWeight: "bolder", fontSize: "25px" }}>{i.title}</td></tr>
+                                            <tr><td style={{ padding: "5px", paddingLeft: "0px", fontWeight: "bold", fontSize: "15px" }}>{i.institute}</td></tr>
+                                            <tr><td style={{ padding: "5px", paddingLeft: "0px" }}>{i.description}</td></tr>
+                                            <tr><td style={{ padding: "5px", paddingLeft: "0px" }}>{i.to_time}</td></tr>
+                                        </table>)}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
